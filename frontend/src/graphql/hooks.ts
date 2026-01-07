7d40b69966f1b3175d4f8c054256ee7a3a22a610
Logan <loganmetsker@yahoo.com>
2026-01-07 10:52:31 -0600

    Disable Apollo/GraphQL - use REST API only

--- frontend/src/graphql/hooks.ts
+++ frontend/src/graphql/hooks.ts
@@ -1,160 +1,29 @@
-import { useQuery, useMutation } from '@apollo/client';
-import {
-  GET_DRIVERS,
-  GET_DRIVER,
-  GET_VEHICLES,
-  GET_VEHICLES_BY_TYPE,
-  GET_VEHICLES_NEEDING_MAINTENANCE,
-  GET_JOBS,
-  GET_ROUTES,
-  GET_ROUTE,
-  GET_CUSTOMERS,
-  GET_CUSTOMER,
-} from './queries';
-import {
-  CREATE_DRIVER,
-  UPDATE_DRIVER,
-  CREATE_VEHICLE,
-  UPDATE_VEHICLE,
-  CREATE_JOB,
-  UPDATE_JOB,
-  CREATE_ROUTE,
-  LOGIN,
-  CREATE_CUSTOMER,
-  UPDATE_CUSTOMER,
-  DELETE_CUSTOMER,
-} from './mutations';
+// GraphQL hooks disabled - using REST API via src/services/api.ts
+// Stub implementations to prevent build errors
 
-// Customer Hooks
-export const useCustomers = () => {
-  return useQuery(GET_CUSTOMERS);
-};
+export const useCustomers = () => ({ data: { customers: [] }, loading: false, error: null });
+export const useCustomer = () => ({ data: null, loading: false, error: null });
+export const useCreateCustomer = () => [() => {}, { loading: false }];
+export const useUpdateCustomer = () => [() => {}, { loading: false }];
+export const useDeleteCustomer = () => [() => {}, { loading: false }];
 
-export const useCustomer = (id: string) => {
-  return useQuery(GET_CUSTOMER, {
-    variables: { id },
-    skip: !id,
-  });
-};
+export const useDrivers = () => ({ data: { drivers: [] }, loading: false, error: null });
+export const useDriver = () => ({ data: null, loading: false, error: null });
+export const useCreateDriver = () => [() => {}, { loading: false }];
+export const useUpdateDriver = () => [() => {}, { loading: false }];
 
-export const useCreateCustomer = () => {
-  return useMutation(CREATE_CUSTOMER, {
-    refetchQueries: [{ query: GET_CUSTOMERS }],
-  });
-};
+export const useVehicles = () => ({ data: { vehicles: [] }, loading: false, error: null });
+export const useVehiclesByType = () => ({ data: { vehicles: [] }, loading: false, error: null });
+export const useVehiclesNeedingMaintenance = () => ({ data: { vehicles: [] }, loading: false, error: null });
+export const useCreateVehicle = () => [() => {}, { loading: false }];
+export const useUpdateVehicle = () => [() => {}, { loading: false }];
 
-export const useUpdateCustomer = () => {
-  return useMutation(UPDATE_CUSTOMER, {
-    refetchQueries: [{ query: GET_CUSTOMERS }],
-  });
-};
+export const useJobs = () => ({ data: { jobs: [] }, loading: false, error: null });
+export const useCreateJob = () => [() => {}, { loading: false }];
+export const useUpdateJob = () => [() => {}, { loading: false }];
 
-export const useDeleteCustomer = () => {
-  return useMutation(DELETE_CUSTOMER, {
-    refetchQueries: [{ query: GET_CUSTOMERS }],
-  });
-};
+export const useRoutes = () => ({ data: { routes: [] }, loading: false, error: null });
+export const useRoute = () => ({ data: null, loading: false, error: null });
+export const useCreateRoute = () => [() => {}, { loading: false }];
 
-// Driver Hooks
-export const useDrivers = () => {
-  return useQuery(GET_DRIVERS);
-};
-
-export const useDriver = (id: string) => {
-  return useQuery(GET_DRIVER, {
-    variables: { id },
-    skip: !id,
-  });
-};
-
-export const useCreateDriver = () => {
-  return useMutation(CREATE_DRIVER, {
-    refetchQueries: [{ query: GET_DRIVERS }],
-  });
-};
-
-export const useUpdateDriver = () => {
-  return useMutation(UPDATE_DRIVER, {
-    refetchQueries: [{ query: GET_DRIVERS }],
-  });
-};
-
-// Vehicle Hooks
-export const useVehicles = () => {
-  return useQuery(GET_VEHICLES);
-};
-
-export const useVehiclesByType = (type: string) => {
-  return useQuery(GET_VEHICLES_BY_TYPE, {
-    variables: { type },
-    skip: !type,
-  });
-};
-
-export const useVehiclesNeedingMaintenance = () => {
-  return useQuery(GET_VEHICLES_NEEDING_MAINTENANCE);
-};
-
-export const useCreateVehicle = () => {
-  return useMutation(CREATE_VEHICLE, {
-    refetchQueries: [
-      { query: GET_VEHICLES },
-      { query: GET_VEHICLES_BY_TYPE, variables: { type: 'truck' } },
-      { query: GET_VEHICLES_BY_TYPE, variables: { type: 'van' } },
-      { query: GET_VEHICLES_BY_TYPE, variables: { type: 'car' } },
-      { query: GET_VEHICLES_NEEDING_MAINTENANCE },
-    ],
-  });
-};
-
-export const useUpdateVehicle = () => {
-  return useMutation(UPDATE_VEHICLE, {
-    refetchQueries: [
-      { query: GET_VEHICLES },
-      { query: GET_VEHICLES_BY_TYPE, variables: { type: 'truck' } },
-      { query: GET_VEHICLES_BY_TYPE, variables: { type: 'van' } },
-      { query: GET_VEHICLES_BY_TYPE, variables: { type: 'car' } },
-      { query: GET_VEHICLES_NEEDING_MAINTENANCE },
-    ],
-  });
-};
-
-// Job Hooks
-export const useJobs = () => {
-  return useQuery(GET_JOBS);
-};
-
-export const useCreateJob = () => {
-  return useMutation(CREATE_JOB, {
-    refetchQueries: [{ query: GET_JOBS }],
-  });
-};
-
-export const useUpdateJob = () => {
-  return useMutation(UPDATE_JOB, {
-    refetchQueries: [{ query: GET_JOBS }],
-  });
-};
-
-// Route Hooks
-export const useRoutes = () => {
-  return useQuery(GET_ROUTES);
-};
-
-export const useRoute = (id: string) => {
-  return useQuery(GET_ROUTE, {
-    variables: { id },
-    skip: !id,
-  });
-};
-
-export const useCreateRoute = () => {
-  return useMutation(CREATE_ROUTE, {
-    refetchQueries: [{ query: GET_ROUTES }],
-  });
-};
-
-// Auth Hooks
-export const useLogin = () => {
-  return useMutation(LOGIN);
-};
+export const useLogin = () => [() => {}, { loading: false }];
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from './apollo/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createAppTheme } from './theme';
@@ -24,14 +22,12 @@ const theme = createAppTheme();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ApolloProvider client={apolloClient}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-          </ThemeProvider>
-        </QueryClientProvider>
-      </ApolloProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
